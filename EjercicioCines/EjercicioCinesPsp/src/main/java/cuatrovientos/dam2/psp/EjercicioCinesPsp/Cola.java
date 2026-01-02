fa5a245868dfea1b240ef69cdca1aacdfc3a72c4
Javier <jrenzullis@gmail.com>
2026-01-02 20:33:19 +0100

    clase Cola completada

--- EjercicioCines/EjercicioCinesPsp/src/main/java/cuatrovientos/dam2/psp/EjercicioCinesPsp/Cola.java
+++ EjercicioCines/EjercicioCinesPsp/src/main/java/cuatrovientos/dam2/psp/EjercicioCinesPsp/Cola.java
@@ -1,5 +1,32 @@
 package cuatrovientos.dam2.psp.EjercicioCinesPsp;
+import java.util.concurrent.Semaphore;
 
 public class Cola {
+	private final int id;
+    private final Semaphore espacioLibre;
+    private final Semaphore clientesEnCola;
+    private int personasActuales = 0;
 
+    public Cola(int id, int capacidadMaxima) {
+        this.id = id;
+        this.espacioLibre = new Semaphore(capacidadMaxima);
+        this.clientesEnCola = new Semaphore(0);
+    }
+
+    public boolean entrar(int idCliente) {
+        if (espacioLibre.tryAcquire()) {
+            synchronized (this) { personasActuales++; }
+            clientesEnCola.release();
+            return true;
+        }
+        return false;
+    }
+
+    public void atender() throws InterruptedException {
+        clientesEnCola.acquire();
+        synchronized (this) { personasActuales--; }
+        espacioLibre.release();
+    }
 }
+
+
